b810b16413dfb1c6c007cc9320af3cc9cce74a8f
MatheusDona <mguellesdona@gmail.com>
2026-08-18 08:31:17 -0300

    Add files via upload

--- Atividade/ListaDeExercicio2/Ex6.c
+++ Atividade/ListaDeExercicio2/Ex6.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	int n, ant, suc;
+	printf("Digite o valor n: ");
+	scanf("%d", &n);
+	suc = n+1;
+	ant = n-1;
+	printf("o numero %d, seu antecesor %d e seu sucessor %d", n, ant, suc);
+	
+	return 0;
+}
